d1fe6ff28dff591b41813594ab235a764a5bf4e2
Shivam <156041072+shiviisbatman@users.noreply.github.com>
2024-08-23 06:10:10 +0000

    Pending changes exported from your codespace

---
+++ addingelementtolist2.py
@@ -0,0 +1,8 @@
+list=[]
+h=int(input("number of elements: "))#ask for number of elements to enter
+for i in range(0,h):#range this will run according to the number of element
+    list1=input("enter:")
+    list=list+[list1]#adding element to list
+    print(list)
+
+print(list)#print final list
