b3134dd44d666f11ab7ecdb6f624a3d12cae28af
Shivam <156041072+shiviisbatman@users.noreply.github.com>
2024-08-23 11:13:59 +0530

    Update toinvesgitate.py

--- toinvesgitate.py
+++ toinvesgitate.py
@@ -2,6 +2,6 @@ list=[]
 h=int(input("number of elements: "))#ask for number of elements to enter
 for i in range(0,h):#range this will run according to the number of element
     list1=input("enter:")
-    list+=list1#adding element to list
+    list+=[list1]#adding element to list
     print(list)
 print(list)#print final list
