1162299955344470bc5d6d79fabf112a2eea707f
Shivam <156041072+shiviisbatman@users.noreply.github.com>
2024-09-04 12:27:38 +0530

    Create minandmaxinlist.py

---
+++ minandmaxinlist.py
@@ -0,0 +1,8 @@
+list_=[]
+n=int(input("Number of elements:"))
+for i in range(1,n):
+    n1=input("input numbers:")
+    list_.append(n1)
+print(list_)
+print("min value is",min(list_))
+print("max value is",max(list_))
